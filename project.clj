0a7394370860de8e6f4396a516266d62ea97eafc
James Reeves <jreeves@weavejester.com>
2011-01-24 11:00:39 +0000

    Generates war, uploads war and creates new app version

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject lein-beanstalk "0.1.0"
   :description "Leiningen plugin for Amazon's Elastic Beanstalk"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [com.amazonaws/aws-java-sdk "1.1.3"]])
+                 [com.amazonaws/aws-java-sdk "1.1.3"]
+                 [lein-ring "0.2.4"]])
--- src/leiningen/beanstalk.clj
+++ src/leiningen/beanstalk.clj
@@ -1,6 +1,58 @@
-(ns leiningen.beanstalk)
+(ns leiningen.beanstalk
+  (:use
+    [leiningen.help :only (help-for)]
+    [leiningen.ring.war :only (war-file-path)]
+    [leiningen.ring.uberwar :only (uberwar)]
+    [clojure.java.io :only (file)])
+  (:import
+    com.amazonaws.auth.BasicAWSCredentials
+    com.amazonaws.services.elasticbeanstalk.AWSElasticBeanstalkClient
+    com.amazonaws.services.elasticbeanstalk.model.CreateApplicationVersionRequest
+    com.amazonaws.services.elasticbeanstalk.model.S3Location
+    com.amazonaws.services.s3.AmazonS3Client))
 
-(defn beanstalk 
+(defn credentials [project]
+  (BasicAWSCredentials.
+    (-> project :aws :access-key)
+    (-> project :aws :secret-key)))
+
+(defn create-version [project]
+  (println "Creating new application version")
+  (.createApplicationVersion
+    (AWSElasticBeanstalkClient. (credentials project))
+    (doto (CreateApplicationVersionRequest.)
+      (.setAutoCreateApplication true)
+      (.setApplicationName (:name project))
+      (.setDescription (:description project))
+      (.setVersionLabel (:version project))
+      (.setSourceBundle (S3Location. "lein-beanstalk" "temp")))))
+
+(defn upload-war [project war-file]
+  (println "Uploading war file to S3")
+  (doto (AmazonS3Client. (credentials project))
+    (.createBucket "lein-beanstalk")
+    (.putObject "lein-beanstalk" "temp" (file war-file))))
+
+(defn deploy-to-environment [project environment])
+
+(defn deploy
+  ([project]
+     (println "Usage: lein beanstalk deploy <environment>"))
+  ([project environment]
+     (let [version  (:version project)
+           war-file (str (:name project) "-" version "-beanstalk.war")]
+       (println "Generating war file")
+       (uberwar project war-file)
+       (upload-war project war-file)
+       (create-version project)
+       (deploy-to-environment project environment))))
+
+(defn beanstalk
   "Manage Amazon's Elastic Beanstalk service."
-  [project]
-  (println "TODO"))
+  {:help-arglists '([deploy])
+   :subtasks [#'deploy]}
+  ([project]
+     (println (help-for "beanstalk")))
+  ([project subtask & args]
+    (case subtask
+      "deploy" (apply deploy project args))))
