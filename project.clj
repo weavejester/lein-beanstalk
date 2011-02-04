3053a7df8c8c950ff89981bf652fdec361796958
ck <ckatgithub@gmail.com>
2011-02-04 13:06:01 -0500

    Added URL to project.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject lein-beanstalk "0.1.0"
   :description "Leiningen plugin for Amazon's Elastic Beanstalk"
+  :url "https://github.com/weavejester/lein-beanstalk"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [com.amazonaws/aws-java-sdk "1.1.3"]
                  [lein-ring "0.2.4"]])
