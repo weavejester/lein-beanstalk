fb04ca79c8dfa5dc3852509ef5fbcb39f8ca6de3
ck <ckatgithub@gmail.com>
2011-02-04 13:55:47 -0500

    More docs as I figure out stuff.

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :url "https://github.com/weavejester/lein-beanstalk"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [com.amazonaws/aws-java-sdk "1.1.3"]
-                 [lein-ring "0.2.4"]])
+                 [lein-ring "0.3.0"]])
