77bda653498ece4e9c1b32e2e63bbc30ff3a9b5f
ck <ckatgithub@gmail.com>
2011-02-08 13:52:37 -0500

    Replace clj-time with standard Java stuff.

--- project.clj
+++ project.clj
@@ -3,5 +3,4 @@
   :url "https://github.com/weavejester/lein-beanstalk"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [com.amazonaws/aws-java-sdk "1.1.5"]
-                 [lein-ring "0.3.2"]
-                 [clj-time "0.3.0-SNAPSHOT"]])
+                 [lein-ring "0.3.2"]])
