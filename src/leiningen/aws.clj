9c99d368e404b40312145876ce0a4be63a74d5e9
James Reeves <jreeves@weavejester.com>
2011-02-09 22:37:02 +0000

    Pretty big refactor of code

--- src/leiningen/aws.clj
+++ src/leiningen/aws.clj
@@ -1,158 +1,96 @@
 (ns leiningen.aws
+  (:require
+    [clojure.java.io :as io]
+    [clojure.string :as str])
+  (:import
+    java.text.SimpleDateFormat
+    java.util.Date
+    com.amazonaws.auth.BasicAWSCredentials
+    com.amazonaws.services.elasticbeanstalk.AWSElasticBeanstalkClient
+    com.amazonaws.services.elasticbeanstalk.model.CreateApplicationVersionRequest
+    com.amazonaws.services.elasticbeanstalk.model.CreateEnvironmentRequest
+    com.amazonaws.services.elasticbeanstalk.model.DeleteApplicationRequest
+    com.amazonaws.services.elasticbeanstalk.model.DescribeEnvironmentsRequest
+    com.amazonaws.services.elasticbeanstalk.model.UpdateEnvironmentRequest
+    com.amazonaws.services.elasticbeanstalk.model.S3Location
+    com.amazonaws.services.s3.AmazonS3Client))
 
-  (:use [clojure.java.io :only (file)]
-        [clojure.string :only (lower-case)])
-
-  (:import com.amazonaws.auth.BasicAWSCredentials
-           com.amazonaws.services.elasticbeanstalk.AWSElasticBeanstalkClient
-           com.amazonaws.services.elasticbeanstalk.model.CreateApplicationVersionRequest
-           com.amazonaws.services.elasticbeanstalk.model.CreateEnvironmentRequest
-           com.amazonaws.services.elasticbeanstalk.model.DeleteApplicationRequest
-           com.amazonaws.services.elasticbeanstalk.model.DescribeEnvironmentsRequest
-           com.amazonaws.services.elasticbeanstalk.model.UpdateEnvironmentRequest
-           com.amazonaws.services.elasticbeanstalk.model.S3Location
-           com.amazonaws.services.s3.AmazonS3Client))
-
-;;
-;; Amazon Web Services functions
-;;
-(defn- credentials
-  "Returns the AWS credentials."
-  [project]
+(defn credentials [project]
   (BasicAWSCredentials.
-   (get-in project [:aws :access-key])
-   (get-in project [:aws :secret-key])))
+    (get-in project [:aws :access-key])
+    (get-in project [:aws :secret-key])))
 
-;;
-;; Helper functions
-;;
-(defonce current-datetime (.format (java.text.SimpleDateFormat. "yyyyMMddHHmmss") (java.util.Date.)))
+(defonce current-timestamp
+  (.format (SimpleDateFormat. "yyyyMMddHHmmss") (Date.)))
 
-(defn create-version
-  "Creates version by appending date & time to project version."
-  [project]
-  (str (:version project) "-" current-datetime))
+(defn app-version [project]
+  (str (:version project) "-" current-timestamp))
 
-(defn versioned-filename
-  "Creates version by appending date & time before the suffix."
-  [project]
-  (str (:name project) "-" (:version project) "-" current-datetime ".war"))
+(defn s3-bucket-name [project]
+  (or (-> project :aws :beanstalk :s3-bucket)
+      (str "lein-beanstalk." (:name project))))
 
-(defn- default-bucket-name
-  "Returns default bucket name for the project."
-  [project]
-  (if-let [bucket-name (get-in project [:aws :s3location])]
-    bucket-name
-    (lower-case (str (:name project) "-" (subs (.getAWSAccessKeyId (credentials project)) 0 9)))))
+(defn s3-upload-file [project filepath]
+  (let [bucket (s3-bucket-name project)
+        file   (io/file filepath)]
+    (doto (AmazonS3Client. (credentials project))
+      (.createBucket bucket)
+      (.putObject bucket (.getName file) file))))
 
-;;
-;; Amazon S3 functions
-;;
-(defn- s3-client
-  [project]
-  (AmazonS3Client. (credentials project)))
-
-(defn- create-bucket
-  "Create S3 bucket. Returns newly created bucket
-   or nil when it already exists.
-
-   Note: The bucket namespace is global, i.e. the bucket might
-   exists, but was created by another user."
-  [project bucket-name]
-  (let [client (s3-client project)]
-    (when-not (.doesBucketExist client bucket-name)
-      (.createBucket client bucket-name))))
-
-(defn upload-file-to-bucket
-  "Uploads the file to the bucket. Returns the
-   uploaded filenname/S3 key.
-
-   Bucket is created if it doesnot exists."
-  ([project filename]
-     (upload-file-to-bucket project filename (default-bucket-name project)))
-  ([project filename bucket-name]
-     (let [client (s3-client project)
-           bucket (create-bucket project bucket-name)
-           obj (file filename)]
-       (.putObject client bucket-name filename obj))))
-
-;;
-;; Amazon Elastic Beanstalk functions
-;;
-(defn- beanstalk-client
-  [project]
+(defn- beanstalk-client [project]
   (AWSElasticBeanstalkClient. (credentials project)))
 
-;; Application
-(defn- create-application-request
-  [project]
-  (doto (CreateApplicationVersionRequest.)
-    (.setApplicationName (:name project))
-    (.setDescription (:description project))))
-
-(defn create-application
+(defn create-app-version
   [project filename]
-  (.createApplication (beanstalk-client project) (create-application-request project)))
-
-(defn- create-application-version-request
-  ([project filename]
-     (create-application-version-request project filename (default-bucket-name project)))
-  ([project filename bucket-name]
-     (doto (CreateApplicationVersionRequest.)
-       (.setAutoCreateApplication true)
-       (.setApplicationName (:name project))
-       (.setVersionLabel (create-version project))
-       (.setDescription (:description project))
-       (.setSourceBundle (S3Location. bucket-name filename)))))
-
-(defn create-application-version
-  [project filename]
-  (.createApplicationVersion (beanstalk-client project) (create-application-version-request project filename)))
-
-(defn describe-applications
-  [project]
-  (.getApplications (.describeApplications (beanstalk-client project))))
+  (.createApplicationVersion
+    (beanstalk-client project)
+    (doto (CreateApplicationVersionRequest.)
+      (.setAutoCreateApplication true)
+      (.setApplicationName (:name project))
+      (.setVersionLabel (app-version project))
+      (.setDescription (:description project))
+      (.setSourceBundle (S3Location. (s3-bucket-name project) filename)))))
 
 (defn get-application
   "Returns the application matching the passed in name"
   [project]
-  (when-let [app (filter #(and (= (.getApplicationName %) (:name project))                               )
-                         (describe-applications project))]
-    (first app)))
+  (->> (beanstalk-client project)
+       .describeApplications
+       .getApplications
+       (filter #(= (.getApplicationName %) (:name project)))
+       first))
 
-;; Environment
-(defn- create-environment-request
-  ([project environment]
-     (create-environment-request project environment "32bit Amazon Linux running Tomcat 6"))
-  ([project environment solution-stack-name]
-     (doto (CreateEnvironmentRequest.)
-       (.setApplicationName (:name project))
-       (.setEnvironmentName environment)
-       (.setVersionLabel (create-version project))
-       (.setSolutionStackName solution-stack-name))))
+(defn create-environment [project environment]
+  (.createEnvironment
+    (beanstalk-client project) 
+    (doto (CreateEnvironmentRequest.)
+      (.setApplicationName (:name project))
+      (.setEnvironmentName environment)
+      (.setVersionLabel (app-version project))
+      (.setSolutionStackName "32bit Amazon Linux running Tomcat 6"))))
 
-(defn- update-environment-request
-  [project environment]
-  (doto (UpdateEnvironmentRequest.)
-    (.setEnvironmentId (.getEnvironmentId environment))
-    (.setEnvironmentName (.getEnvironmentName environment))
-    (.setVersionLabel (create-version project))))
+(defn update-environment [project environment]
+  (.updateEnvironment
+    (beanstalk-client project)
+    (doto (UpdateEnvironmentRequest.)
+      (.setEnvironmentId (.getEnvironmentId environment))
+      (.setEnvironmentName (.getEnvironmentName environment))
+      (.setVersionLabel (app-version project)))))
 
-(defn describe-environments
-  [project]
-  (filter #(and (= (.getApplicationName %) (:name project)))
-          (.getEnvironments (.describeEnvironments (beanstalk-client project)))))
+(defn app-environments [project]
+  (->> (beanstalk-client project)
+      .describeEnvironments
+      .getEnvironments
+      (filter #(= (.getApplicationName %) (:name project)))))
 
-(defn get-environment
-  "Returns the environment matching the passed in name"
-  [project environment-name]
-  (when-let [envs (filter #(and (= (.getApplicationName %) (:name project))
-                                (= (.getEnvironmentName %) environment-name))
-                          (describe-environments project))]
-    (first envs)))
+(defn get-environment [project environment]
+  (->> (app-environments project)
+       (filter #(and (= (.getEnvironmentName %) environment)
+                     (= (.getStatus %) "Ready")))
+       first))
 
 (defn deploy-environment
-  [project environment-name]
-  (if-let [env (get-environment project environment-name)]
-    (.updateEnvironment (beanstalk-client project) (update-environment-request project env))
-    (.createEnvironment (beanstalk-client project) (create-environment-request project environment-name))))
+  [project env-name]
+  (if-let [env (get-environment project env-name)]
+    (update-environment project env)
+    (create-environment project env-name)))
--- src/leiningen/beanstalk.clj
+++ src/leiningen/beanstalk.clj
@@ -1,14 +1,16 @@
 (ns leiningen.beanstalk
+  (:require [leiningen.aws :as aws]
+            [clojure.string :as str])
   (:use [leiningen.help :only (help-for)]
         [leiningen.ring.war :only (war-file-path)]
-        [leiningen.ring.uberwar :only (uberwar)]
-        [clojure.java.io :only (file)]
-        [clojure.string :only (join)]
-        [leiningen.aws :as aws]))
+        [leiningen.ring.uberwar :only (uberwar)]))
 
 (defn- project-env-exists? [project environment]
   (let [envs (-> project :aws :beanstalk :environments)]
     (contains? (set (map str envs)) environment)))
+
+(defn war-filename [project]
+  (str (:name project) "-" (aws/app-version project) ".war"))
 
 (defn deploy
   "Deploy the current project to Amazon Elastic Beanstalk."
@@ -17,52 +19,70 @@
   ([project environment]
      (if-not (project-env-exists? project environment)
        (println (str "Environment '" environment "' not in project.clj"))
-       (let [filename (versioned-filename project)]
+       (let [filename (war-filename project)]
          (uberwar project filename)
-         (aws/upload-file-to-bucket project filename)
-         (aws/create-application-version project filename)
+         (aws/s3-upload-file project filename)
+         (aws/create-app-version project filename)
          (aws/deploy-environment project environment)))))
+
+(def app-info-indent "\n                  ")
+
+(defn- last-versions-info [app]
+  (str/join app-info-indent (take 5 (.getVersions app))))
+
+(defn- deployed-envs-info [project]
+  (str/join
+    app-info-indent
+    (for [env (aws/app-environments project)]
+      (str (.getEnvironmentName env) " (" (.getStatus env) ")"))))
+
+(defn app-info
+  "Displays information about a Beanstalk application."
+  [project]
+  (if-let [app (aws/get-application project)]
+    (println (str "Application Name: " (.getApplicationName app) "\n"
+                  "Last 5 Versions:  " (last-versions-info app) "\n"
+                  "Created On:       " (.getDateCreated app) "\n"
+                  "Updated On:       " (.getDateUpdated app) "\n"
+                  "Deployed Envs:    " (deployed-envs-info project)))
+    (println (str "Application '" (:name project) "' "
+                  "not found on AWS Elastic Beanstalk"))))
+
+(defn env-info
+  "Displays information about a Beanstalk environment."
+  [project env-name]
+  (if-let [env (aws/get-environment project env-name)]    
+    (println (str "Environment ID:    " (.getEnvironmentId env) "\n"
+                  "Application Name:  " (.getApplicationName env) "\n"
+                  "Environment Name:  " (.getEnvironmentName env) "\n"
+                  "Description:       " (.getDescription env) "\n"
+                  "URL:               " (.getCNAME env) "\n"
+                  "Load Balancer URL: " (.getEndpointURL env) "\n"
+                  "Status:            " (.getStatus env) "\n"
+                  "Health:            " (.getHealth env) "\n"
+                  "Current Version:   " (.getVersionLabel env) "\n"
+                  "Solution Stack:    " (.getSolutionStackName env) "\n"
+                  "Created On:        " (.getDateCreated env) "\n"
+                  "Updated On:        " (.getDateUpdated env)))
+    (println (str "Environment '" env-name "' "
+                  "not found on AWS Elastic Beanstalk"))))
 
 (defn info
   "Provides info for about project on Amazon Elastic Beanstalk."
   ([project]
-     (if-let [app (aws/get-application project)]
-       (do
-         (println (str "Application Name : " (.getApplicationName app)))
-         (println (str "Description      : " (.getDescription app)))
-         (println (str "Last 5 Versions  : " (join "\n                   " (take 5 (.getVersions app)))))
-         (println (str "Created On       : " (.getDateCreated app)))
-         (println (str "Updated On       : " (.getDateUpdated app)))
-         (println (str "Deployed Envs    : " (join "\n                   "
-                                                   (map #(str (.getEnvironmentName %) " (" (.getStatus %) ")")
-                                                        (aws/describe-environments project))))))
-       (println (str "Application '" (:name project) "' not found on AWS Elastic Beanstalk!"))))
-  ([project environment-name]
-     (if-not (project-env-exists? project environment-name)
-       (println (str "Environment '" environment-name "' not in project.clj!"))
-       (if-let [env (aws/get-environment project environment-name)]
-         (do
-           (println (str "Environment Id   : " (.getEnvironmentId env)))
-           (println (str "Application Name : " (.getApplicationName env)))
-           (println (str "Environment Name : " (.getEnvironmentName env)))
-           (println (str "Description      : " (.getDescription env)))
-           (println (str "URL              : " (.getCNAME env)))
-           (println (str "LoadBalancer URL : " (.getEndpointURL env)))
-           (println (str "Status           : " (.getStatus env)))
-           (println (str "Health           : " (.getHealth env)))
-           (println (str "Current Version  : " (.getVersionLabel env)))
-           (println (str "Solution Stack   : " (.getSolutionStackName env)))
-           (println (str "Created On       : " (.getDateCreated env)))
-           (println (str "Updated On       : " (.getDateUpdated env))))
-         (println (str "Environment '" environment-name "' not found on AWS Elastic Beanstalk!"))))))
+     (app-info project))
+  ([project env-name]
+     (if-not (project-env-exists? project env-name)
+       (println (str "Environment '" env-name "' not in project.clj!"))
+       (env-info project env-name))))
 
 (defn beanstalk
   "Manage Amazon's Elastic Beanstalk service."
-  {:help-arglists '([deploy] info)
+  {:help-arglists '([deploy info])
    :subtasks [#'deploy #'info]}
   ([project]
      (println (help-for "beanstalk")))
   ([project subtask & args]
     (case subtask
-          "deploy" (apply deploy project args)
-          "info"   (apply info project args))))
+      "deploy" (apply deploy project args)
+      "info"   (apply info project args))))
