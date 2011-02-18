9c1289d790ee711d6202af854e6f524f678d6cb7
ck <ckatgithub@gmail.com>
2011-02-18 08:33:58 -0500

    Updated aws-java-sdk to latest version (1.1.6).

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "Leiningen plugin for Amazon's Elastic Beanstalk"
   :url "https://github.com/weavejester/lein-beanstalk"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [com.amazonaws/aws-java-sdk "1.1.5"]
+                 [com.amazonaws/aws-java-sdk "1.1.6"]
                  [lein-ring "0.3.2"]])
