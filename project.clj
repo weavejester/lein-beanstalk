678c876fcfbd8797a04446fdf33ed6864dafce4b
James Reeves <jreeves@weavejester.com>
2012-08-15 10:38:05 +0100

    Added eval-in-leiningen to project.clj

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/weavejester/lein-beanstalk"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [com.amazonaws/aws-java-sdk "1.1.7"]
-                 [lein-ring "0.7.1"]])
+                 [lein-ring "0.7.1"]]
+  :eval-in-leiningen true)
