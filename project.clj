720f0718ad993f6412b0c707845fb1d78f5ac2ee
James Reeves <jreeves@weavejester.com>
2012-08-15 10:41:08 +0100

    Updated dependency versions

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject lein-beanstalk "0.2.3-SNAPSHOT"
   :description "Leiningen plugin for Amazon's Elastic Beanstalk"
   :url "https://github.com/weavejester/lein-beanstalk"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [com.amazonaws/aws-java-sdk "1.1.7"]
-                 [lein-ring "0.7.1"]]
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [com.amazonaws/aws-java-sdk "1.3.14"]
+                 [lein-ring "0.7.2"]]
   :eval-in-leiningen true)
