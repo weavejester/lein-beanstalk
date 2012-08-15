65389d7bb6a58b6479f46451a4b1bba048756326
James Reeves <jreeves@weavejester.com>
2012-08-15 10:37:25 +0100

    Removed lein-clojars from project.clj

--- project.clj
+++ project.clj
@@ -3,5 +3,4 @@
   :url "https://github.com/weavejester/lein-beanstalk"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [com.amazonaws/aws-java-sdk "1.1.7"]
-                 [lein-ring "0.7.1"]]
-  :dev-dependencies [[lein-clojars "0.7.0"]])
+                 [lein-ring "0.7.1"]])
