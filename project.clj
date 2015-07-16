d9e86f88428bdfb51e99b9bea4280f58ff9c93a6
Martin Kremers <info@martinkremers.de>
2015-07-16 20:45:41 +0200

    Remove clojure from dependencies since this is a plugin

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject lein-beanstalk "0.2.7"
   :description "Leiningen plugin for Amazon's Elastic Beanstalk"
   :url "https://github.com/weavejester/lein-beanstalk"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [com.amazonaws/aws-java-sdk "1.6.10"]
+  :dependencies [[com.amazonaws/aws-java-sdk "1.6.10"]
                  [lein-ring "0.8.2"]]
   :eval-in-leiningen true)
