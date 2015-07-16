a61b2621be709ecd7c67f4e02d5cbd59145902ea
Martin Kremers <info@martinkremers.de>
2015-07-16 20:46:39 +0200

    Update lein-ring dependency to 0.9.6

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "Leiningen plugin for Amazon's Elastic Beanstalk"
   :url "https://github.com/weavejester/lein-beanstalk"
   :dependencies [[com.amazonaws/aws-java-sdk "1.6.10"]
-                 [lein-ring "0.8.2"]]
+                 [lein-ring "0.9.6"]]
   :eval-in-leiningen true)
